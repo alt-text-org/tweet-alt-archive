a50ee8555d1c47af03a517c576d983e9580ddf9d
Hannah Kolbeck <hskolbeck@gmail.com>
2022-11-07 10:28:50 -0800

    First go at things

--- server.js
+++ server.js
@@ -1,32 +1,161 @@
-import path from 'path';
-// Require the fastify framework and instantiate it
-import {fastify as fast} from "fastify";
+const path = require('path')
+const crypto = require("crypto");
 
-import generated from "@fastify/static";
+const fastify = require('fastify')({logger: true});
+const {auth} = require("twitter-api-sdk");
+const {v4} = require("uuid")
+const fs = require("fs");
+const child_process = require("child_process");
 
-const fastify = fast({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname, 'public'),
+    prefix: '/public/',
+})
 
-// Setup our static files
-fastify.register(generated, {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
+const config = {
+    twitter: {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET
+    }
+}
+const twtr = new auth.OAuth2User({
+    client_id: config.twitter.client_id,
+    client_secret: config.twitter.client_secret,
+    callback: "https://archive.alt-text.org/callback",
+    scopes: ["tweet.read", "users.read", "offline.access"],
 });
 
 fastify.get("/", function (request, reply) {
+    reply.sendFile("index.html")
 });
+
+fastify.get("/archive", function (request, reply) {
+    reply.sendFile("archive.html")
+});
+
+fastify.get("/download", function (request, reply) {
+    reply.sendFile("download.html")
+});
+
+const twitterStates = {};
+const MAX_STATE_LIFETIME_MILLIS = 5 * 60 * 1000;
+
+function cleanTwitterStates() {
+    for (let state in twitterStates) {
+        let ts = twitterStates[state];
+        if (Date.now() - ts > MAX_STATE_LIFETIME_MILLIS * 3) {
+            console.log(`${ts}: Deleting expired state`);
+            delete twitterStates[state];
+        }
+    }
+}
+
+const signupOpts = {
+    handler: (request, reply) => {
+        const state = crypto.randomBytes(16).toString("base64");
+        twitterStates[state] = Date.now();
+
+        const authUrl = twtr.generateAuthURL({
+            state: state, code_challenge_method: "plain", code_challenge: crypto.randomBytes(64).toString("base64"),
+        });
+
+        const body = {
+            auth_url: authUrl,
+        };
+
+        reply.status(200).send(JSON.stringify(body));
+    },
+};
+fastify.get("/api/get-auth-url", signupOpts);
+
+const signupCallbackOpts = {
+    schema: {
+        querystring: {
+            type: "object",
+            properties: {
+                url: {
+                    type: "string",
+                },
+            },
+        },
+    },
+    handler: async (request, reply) => {
+        const {code, state} = request.query;
+
+        const stateIssueTime = twitterStates[state];
+        if (!stateIssueTime) {
+            reply.status(400).send({error: "Unknown state"});
+        } else if (Date.now() - stateIssueTime > MAX_STATE_LIFETIME_MILLIS) {
+            reply.status(400).send({
+                error: `State expired, must hit callback within ${MAX_STATE_LIFETIME_MILLIS} milliseconds`,
+            });
+        }
+
+        delete twitterStates[state];
+
+        reply.redirect(307, `/archive?code-${code}`);
+    }
+};
+fastify.get("/callback", signupCallbackOpts);
+
+
+const uploadOpts = {
+    schema: {
+        body: {
+            type: "object",
+            required: ["tweet_ids", "code"],
+            properties: {
+                tweet_ids: {
+                    type: "array",
+                    items: {
+                        type: "string"
+                    }
+                },
+                code: {
+                    type: "string"
+                }
+            },
+        },
+    },
+    handler: async (request, reply) => {
+        const {tweet_ids, code} = request.body;
+        const uuid = v4();
+
+        let file = `/in-progress/${uuid}.json`;
+        fs.writeFileSync(file, tweet_ids);
+        const child = child_process.spawn(`node task.js ${uuid} ${code} ${file}`);
+        child.stdout.on("data", data => {
+            console.log(`${uuid}: ${data}`);
+        });
+
+        child.stderr.on("data", data => {
+            console.log(`${uuid}: ${data}`);
+        });
+
+        child.on('error', (error) => {
+            console.log(`spawn error: ${error.message}`);
+        });
+
+        child.on("close", code => {
+            console.log(`child process ${uuid} exited with code ${code}`);
+        });
+
+        reply.status(200).send({uuid});
+    }
+};
+fastify.post("/api/upload", uploadOpts);
+
+setInterval(cleanTwitterStates, 10000)
 
 // Run the server and report out to the logs
 fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
+    {port: process.env.PORT, host: "0.0.0.0"},
+    function (err, address) {
+        if (err) {
+            fastify.log.error(err);
+            process.exit(1);
+        }
+        console.log(`Your app is listening on ${address}`);
+        fastify.log.info(`server listening on ${address}`);
     }
-    console.log(`Your app is listening on ${address}`);
-    fastify.log.info(`server listening on ${address}`);
-  }
 );
---
+++ task.js
@@ -0,0 +1,134 @@
+const fs = require("fs");
+const {Client, auth} = require("twitter-api-sdk");
+const {Storage} = require("@google-cloud/storage");
+const stream = require('stream');
+
+const args = process.argv.slice(2);
+const [uuid, code, tweetFile] = args
+
+const config = {
+    gcs_token: process.env.GCS_TOKEN,
+    twitter: {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET
+    }
+}
+
+if (tweetFile) {
+    task(config, uuid, code, tweetFile)
+        .catch(async err => {
+            console.log(err)
+            await error(`Task failed: ${err}`, uuid)
+        });
+} else {
+    error("Internal error, task was not invoked correctly", uuid).catch()
+}
+
+async function task(config, uuid, code, tweetFile) {
+    const twtr = await makeTwitterClient(config);
+    const tweets = fs.readFileSync(tweetFile).toJSON();
+
+    const alt = [];
+    for (let i = 0; i < tweets.length; i += 100) {
+        let slice = tweets.slice(i, i + 100);
+        if (slice.length > 0) {
+            (await getTweets(twtr, slice)).forEach(tweet => alt.push(tweet));
+        }
+    }
+
+    saveToGcs(`/${uuid}/result.json`, alt);
+}
+
+async function getTweets(twtr, tweetIds) {
+    let sleep = 1000
+    while (true) {
+        let tweets = await tryGetTweets(twtr, tweetIds).catch(err => console.log(err))
+        if (tweets) {
+            return tweets
+        }
+
+        await new Promise(r => setTimeout(r, sleep));
+        sleep = Math.min(15000, sleep + 1000)
+    }
+}
+
+
+async function tryGetTweets(twtr, tweetIds) {
+    let tweets = await twtr.tweets.findTweetsById({
+        ids: tweetIds,
+        expansions: ["attachments.media_keys"],
+        "media.fields": ["media_key", "url", "alt_text"],
+        "tweet.fields": ["entities", "lang", "attachments"]
+    })
+
+    let mediaKeyToTweetId = {}
+    tweets.data.forEach(tweet => {
+        if (tweet.attachments && tweet.attachments.media_keys) {
+            tweet.attachments.media_keys.forEach(key => mediaKeyToTweetId[key] = tweet.id)
+        }
+    })
+
+    const imagesAndAlts = [];
+    if (tweets.includes && tweets.includes.media) {
+        tweets.includes.media.forEach((media) => {
+            if ((media.type === "photo" || media.type === "animated_gif") && media.alt_text) {
+                imagesAndAlts.push({
+                    tweet_id: mediaKeyToTweetId[media.media_key],
+                    media_key: media.media_key,
+                    media_url: media.url,
+                    alt_text: media.alt_text,
+                });
+            }
+        });
+    }
+
+    return imagesAndAlts;
+}
+
+function saveToGcs(path, contents) {
+    if (typeof contents !== "object") {
+        contents = JSON.stringify(contents);
+    }
+
+    const gcs = new Storage({
+        projectId: "alt-text-org",
+        keyFilename: config.gcs_token
+    });
+
+    const bucket = gcs.bucket("download.alt-text.org");
+    const file = bucket.file(path);
+
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(Buffer.from(contents));
+    bufferStream.pipe(file.createWriteStream({
+        metadata: {
+            contentType: 'application/json',
+        },
+        public: true,
+        validation: "md5"
+    })).on('error', function (err) {
+        console.log(err)
+    }).on('finish', function () {
+        file.makePublic()
+    });
+}
+
+async function makeTwitterClient(config, code) {
+    const myClient = new auth.OAuth2User({
+        client_id: config.twitter.client_id,
+        client_secret: config.twitter.client_secret,
+        callback: "https://archive.alt-text.org/callback",
+        scopes: ["tweet.read", "users.read", "offline.access"],
+    });
+
+    const tokenWrapper = await myClient.requestAccessToken(code);
+    if (tokenWrapper && tokenWrapper.token && tokenWrapper.token.access_token) {
+        return new Client(new auth.OAuth2Bearer(tokenWrapper.token.access_token))
+    } else {
+        throw new Error("Couldn't get Twitter client")
+    }
+}
+
+async function error(msg, uuid) {
+    saveToGcs(`/${uuid}/error.json`, msg)
+}
